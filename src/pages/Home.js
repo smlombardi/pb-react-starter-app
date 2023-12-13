15ab5736eca1666be6785ee1c262ba400aaec347
Steve Lombardi <smlombardi@gmail.com>
2023-12-13 13:27:54 -0500

    add DS footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,17 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, dicta
-      facilis earum repellat explicabo, quasi tenetur non quo odio dolor illum?
-      Hic dolorem, explicabo omnis nemo asperiores provident unde error rerum
-      vitae. Quasi cumque amet, ad labore nostrum expedita vero consectetur nam
-      aut asperiores maxime. Sed itaque expedita quos assumenda!
+    <div className="container mt-7">
+      <div className="row">
+        <div className="col">
+          <h1>Home</h1>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab, dicta
+          facilis earum repellat explicabo, quasi tenetur non quo odio dolor
+          illum? Hic dolorem, explicabo omnis nemo asperiores provident unde
+          error rerum vitae. Quasi cumque amet, ad labore nostrum expedita vero
+          consectetur nam aut asperiores maxime. Sed itaque expedita quos
+          assumenda!
+        </div>
+      </div>
     </div>
   );
 }
