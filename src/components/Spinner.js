803593a4f4bfd07871b69bf70e662e99879e15ff
Steve Lombardi <smlombardi@gmail.com>
2023-12-30 09:52:45 -0500

    spinner
    Signed-off-by: Steve Lombardi <smlombardi@gmail.com>

---
+++ src/components/Spinner.js
@@ -0,0 +1,18 @@
+export default function Spinner({ size = 48 }) {
+  return (
+    <>
+      <span role="alert" aria-live="assertive">
+        <span
+          className="sbl-circ"
+          style={{
+            width: `${size}px`,
+            height: `${size}px`,
+            borderWidth: `${size / 8}px`,
+          }}
+          aria-hidden="true"
+        ></span>
+        <span className="visually-hidden">Loading</span>
+      </span>
+    </>
+  );
+}
